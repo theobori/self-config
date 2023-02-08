88ce24df98504a14c4b609a31332e9eaf088a998
Théo Bori <theo1.bori@epitech.eu>
2023-02-08 03:52:43 +0100

    [ADD] Added the `fullenv` role
    Containing wezterm (with config files)

---
+++ roles/fullenv/files/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require 'wezterm'
+
+return {
+    color_scheme = "Dracula (Official)",
+    tab_bar_at_bottom = false,
+    use_fancy_tab_bar = false,
+    window_decorations = "RESIZE",
+    hide_tab_bar_if_only_one_tab = true,
+    default_prog = { "tmux" },
+    font = wezterm.font_with_fallback {
+        "Comic Code",
+        "JetBrains Mono",
+        "Noto Color Emoji"
+    },
+    font_size = 11
+}
